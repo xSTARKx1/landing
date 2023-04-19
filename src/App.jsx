2bf73645deee83f17fafee25e01c2c275a459ead
xSTARKx1 <victor.k9560@gmail.com>
2023-04-19 18:54:35 +0300

    animation for main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Fade from 'react-reveal/Fade';
+
 import {
   AboutUs,
   Icons,
@@ -7,31 +9,38 @@ import {
   Navigation,
   Slider,
 } from './components';
-import image from './assets/title.svg';
+import blueLine from './assets/line.svg';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className='app'>
-      <header className='app__header'>
-        <Logo />
-        <Navigation />
-        <button className='app__header-button'>Buy</button>
-      </header>
-      <main>
-        <div className='app__banner-section'>
-          <div className='app__banner-section--text-wrapper'>
-            <Icons />
-            <MainTitle />
+    <div className='app-wrapper'>
+      <img src={blueLine} alt='blue line' className='line' />
+      <div className='app'>
+        <header className='app__header'>
+          <Logo />
+          <Navigation />
+          <button className='app__header-button'>Buy</button>
+        </header>
+        <main>
+          <div className='app__banner-section'>
+            <div className='app__banner-section--text-wrapper'>
+              <Icons />
+              <MainTitle />
+            </div>
+            <ImageTitle />
           </div>
-          <ImageTitle />
-        </div>
-        <section className='app__main-section'>
-          <Slider />
-          <AboutUs />
-        </section>
-      </main>
+          <Fade>
+            <section className='app__main-section'>
+              <Slider />
+              <Fade bottom>
+                <AboutUs />
+              </Fade>
+            </section>
+          </Fade>
+        </main>
+      </div>
     </div>
   );
 }
